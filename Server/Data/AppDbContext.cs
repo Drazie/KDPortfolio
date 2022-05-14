55422cf913e19fcd88da4f64f0fba8bcccd66dee
Piotr Kleszczewski <mailto:piotr.kleszczewski@assetbook.com>
2022-05-14 04:03:55 +0300

    Added DbContext

---
+++ Server/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            Category[] categoriesToSeed = new Category[3];
+            for (int i = 1; i < 4; i++)
+            {
+                categoriesToSeed[i - 1] = new Category
+                {
+                    Id = i,
+                    ThumbnailImagePath = "uploads/myplaceholder.jpg",
+                    Name = $"Category {i}",
+                    Description = $"Description of the Category {i}"
+                };
+            }
+
+            modelBuilder.Entity<Category>().HasData(categoriesToSeed);
+        }
+    }
+}
